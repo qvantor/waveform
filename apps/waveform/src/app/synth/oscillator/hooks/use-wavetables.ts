d1b601d5e57ab9b6acf038ba6c989d0bd6d0f37b
sergeynikolaev <iintersergey@gmail.com>
2023-01-12 20:01:07 +0300

    wavetables size support

--- apps/waveform/src/app/synth/oscillator/hooks/use-wavetables.ts
+++ apps/waveform/src/app/synth/oscillator/hooks/use-wavetables.ts
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { CascadeOption } from '@waveform/ui-kit';
+import prettyBytes from 'pretty-bytes';
 
 interface Wavetable {
   Key: string;
@@ -18,7 +19,7 @@ const wavetablesToOptions = (data: Element[]): CascadeOption[] =>
     if ('Key' in wave) {
       const path = wave.Key.split('/');
       const [filename] = path[path.length - 1].split('.');
-      return { value: wave.Key, label: filename };
+      return { value: wave.Key, label: `${filename} (${prettyBytes(wave.Size)})` };
     }
     return { value: wave.name, label: wave.name, children: wavetablesToOptions(wave.children) };
   });
@@ -26,8 +27,7 @@ const wavetablesToOptions = (data: Element[]): CascadeOption[] =>
 export const useWavetables = () => {
   const { data } = useQuery<Element[]>({
     queryKey: ['wavetables'],
-    queryFn: () =>
-      fetch('/.netlify/functions/get-wavetables-list').then((res) => res.json()),
+    queryFn: () => fetch('/.netlify/functions/get-wavetables-list').then((res) => res.json()),
     staleTime: 24 * 60 * 60 * 1000,
   });
   return wavetablesToOptions(data ?? []);
