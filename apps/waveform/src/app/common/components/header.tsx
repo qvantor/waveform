a04bb663c0f4bc155c808d29c9de1b22fb492e53
sergeynikolaev <iintersergey@gmail.com>
2023-04-19 15:07:41 +0300

    link to github added

--- apps/waveform/src/app/common/components/header.tsx
+++ apps/waveform/src/app/common/components/header.tsx
@@ -11,23 +11,42 @@ const Root = styled.div`
   justify-content: space-between;
 `;
 
+const Logo = styled.a`
+  text-decoration: none;
+  border-right: 1px solid ${theme.colors.primaryLowContrast};
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-right: 20px;
+`;
+
 const Name = styled.h1`
   color: ${theme.colors.secondAccent};
   font-size: 36px;
-  line-height: 40px;
   font-weight: 700;
   background: -webkit-linear-gradient(45deg, ${theme.colors.secondAccent}, ${theme.colors.accent} 80%);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
-  padding: 20px 20px 20px 0;
   margin: 0;
-  border-right: 1px solid ${theme.colors.primaryLowContrast};
+`;
+
+const Small = styled.small`
+  display: block;
+  font-size: 12px;
+  text-align: right;
+  color: ${theme.colors.primaryDarkMediumContrast};
+  font-weight: bold;
+  margin-top: -6px;
 `;
 
 export const Header = ({ children }: React.PropsWithChildren) => {
   return (
     <Root>
-      <Name>WAVEFORM</Name>
+      <Logo href='https://github.com/qvantor/waveform' target='_blank' referrerPolicy='no-referrer'>
+        <Name>WAVEFORM</Name>
+        <Small>by Qvantor</Small>
+      </Logo>
       {children}
     </Root>
   );
